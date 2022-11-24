1866293f4a775637167fd988a2a42ef5fd0d219b
MohdEhtesham <ehteshamidrishi@gmail.com>
2022-11-24 16:36:53 +0530

    stop watch

--- todo/app.js
+++ todo/app.js
@@ -1,13 +1,1 @@
-
-var addButton = document.querySelector(".todo-button");
-
-let previoustodo = [];
-
-addButton.addEventListener("click", ()=>{
-    var Input = document.querySelector("todo-input").value;
-    previoustodo.push(Input)
-
-    const pbmis = document.querySelector(".todo-list").innerHTML =(previoustodo);
-       
-  
-});
+console.log("ehteshambhai")
