4a38a7dd0dd7ba7fac9a8d9b26d697937a357642
MohdEhtesham <ehteshamidrishi@gmail.com>
2022-11-24 21:17:32 +0530

    rgb color picker

---
+++ rgb/rgb.js
@@ -0,0 +1,29 @@
+function colorpicker() {
+
+
+
+    var firstcolor = document.getElementById("red");
+    var secondcolor = document.getElementById("green");
+    var thirdcolor = document.getElementById("blue");
+    let redcolor = document.getElementById('Red');
+    let greencolor = document.getElementById('Green');
+    let bluecolor = document.getElementById('Blue');
+
+
+    redcolor.innerHTML = firstcolor.value;
+    var redvalue = firstcolor.value
+
+    greencolor.innerHTML = secondcolor.value;
+    var greenvalue = secondcolor.value
+
+    bluecolor.innerHTML = thirdcolor.value;
+    var bluevalue = thirdcolor.value
+
+
+
+
+  var mixvalue =   "rgb("+ redvalue +","+ greenvalue +","+ bluevalue +")";
+
+console.log(mixvalue)
+document.getElementById("block").style.background =mixvalue;
+}
