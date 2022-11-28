bcb0a6638db3f026ede20484448c088a16f2fb54
MohdEhtesham <ehteshamidrishi@gmail.com>
2022-11-28 14:58:47 +0530

    todo implementation in remove

--- todo/todo.js
+++ todo/todo.js
@@ -1,4 +1,4 @@
-
+let row=0;
 const button = document.getElementById("todobutton")
 
 button.addEventListener('click',()=>{
@@ -6,8 +6,9 @@ button.addEventListener('click',()=>{
  document.getElementById("textfield").value=""
  if(val.length!=0){
     var listitem =document.createElement('li')
-    listitem.innerText=val
-    listitem.id =val
+    // listitem.innerText=val
+    // listitem.id =val
+    
     listitem.addEventListener('click',()=>{
         if(listitem.style.backgroundColor=='green'){
             listitem.style.backgroundColor='white';
@@ -16,11 +17,27 @@ button.addEventListener('click',()=>{
             listitem.style.backgroundColor='green'
 
         }
+        
     })
+    
+ 
+    // var list= document.getElementById("todo-list")
+    // list.appendChild(listitem)
+    var textnode=document.createTextNode(val);
+    listitem.appendChild(textnode)
+    listitem.setAttribute("id","contentP"+row);
+    document.getElementById('todo-list').appendChild(listitem);
+    var removeTask = document.createElement('input');
+    removeTask.setAttribute('type', 'button');
+    removeTask.setAttribute("value", "X");
+    removeTask.setAttribute("id", "removeButton");
+    removeTask.setAttribute("onClick", "deleterow("+ row +");");
+
+    listitem.appendChild(removeTask);
+        row++;
    
-    var list= document.getElementById("todo-list")
-    list.appendChild(listitem)
  }
+ 
  
  
 })
@@ -30,3 +47,13 @@ const buttton2 =document.getElementById("reset-button")
 buttton2.addEventListener('click',()=>{
     document.getElementById("todo-list").innerHTML=" "
 })
+
+
+function deleterow(ID)
+{
+    document.getElementById('contentP'+ID).remove();
+}
+localStorage.setItem("firstName", "Mohd");
+localStorage.setItem("LastName", "Ehtesham");
+document.getElementById("demo").innerHTML = localStorage.getItem("firstName");
+document.getElementById("demo").innerHTML = localStorage.getItem("LastName");
